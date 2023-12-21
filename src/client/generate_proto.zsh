a6cfe95c97e75f91cdeb1c3d4d9a05d9362b3a03
Siddharth Bansal <82485739+KANNAHWORLD@users.noreply.github.com>
2023-12-20 17:41:47 -0800

    Files

---
+++ src/client/generate_proto.zsh
@@ -0,0 +1,9 @@
+
+# This script generates the python files from the proto files.
+# directory is the directory where the proto files are located.
+directory="../protos/"
+build_directory="./src/rpc/"
+
+rm -rf $build_directory
+mkdir -p $build_directory
+python3 -m grpc_tools.protoc -I../protos --python_out=$build_directory --pyi_out=$build_directory --grpc_python_out=$build_directory $directory/*.proto
